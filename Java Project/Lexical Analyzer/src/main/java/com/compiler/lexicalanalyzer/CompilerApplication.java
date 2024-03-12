c8fd41914ee6344f359b0c15b020bc05ae59c18a
MaeenMo <2101544@eng.asu.edu.eg>
2024-03-12 02:51:14 +0200

    update: gui theme and design, fix: line numbers not align correctly with code lines, fix: issues in tables display, fix: resizing issues

--- Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/CompilerApplication.java
+++ Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/CompilerApplication.java
@@ -16,7 +16,8 @@ public class CompilerApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
+        stage.setMinHeight(660);
+        stage.setMinWidth(960);
         FXMLLoader fxmlLoader = new FXMLLoader(CompilerApplication.class.getResource("Compiler.fxml"));
         stage.getIcons().add(new Image(Objects.requireNonNull(CompilerApplication.class.getResourceAsStream("Clanguage.png"))));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
--- Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/CompilerController.java
+++ Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/CompilerController.java
@@ -1,9 +1,11 @@
 package com.compiler.lexicalanalyzer;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -15,16 +17,17 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-import java.util.List;
+
+import static java.lang.Long.MAX_VALUE;
 
 public class CompilerController implements Initializable {
 
     FileChooser fileChooser = new FileChooser();
     LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
     File file;
+    File previousFile = null;
     @FXML
     private TextArea codeArea;
     @FXML
@@ -37,12 +40,17 @@ public class CompilerController implements Initializable {
     @FXML
     void readCode(MouseEvent event) {
         file = fileChooser.showOpenDialog(new Stage());
-        if (file == null) {
+        if (file != null)
+            previousFile = file;
+        else if(previousFile == null)
+            return;
+        else{
+            file = previousFile;
             return;
         }
         try {
             Scanner scanner = new Scanner(file);
-            if (!codeArea.equals("")){
+            if (!codeArea.equals("")) {
                 lineArea.clear();
                 codeArea.clear();
                 viewWindow.getStyleClass().add("hideView");
@@ -52,15 +60,14 @@ public class CompilerController implements Initializable {
             int i = 0;
             while (scanner.hasNextLine()) {
                 i++;
-                lineArea.appendText(i+"\n");
-                codeArea.appendText(scanner.nextLine()+"\n");
+                lineArea.appendText(i + "\n");
+                codeArea.appendText(scanner.nextLine() + "\n");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-
         }
     }
-String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", "String", "Char", "Punctuation"};
+    String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", "String", "Char", "Punctuation"};
     @FXML
     void scanCode(MouseEvent event) {
         if (file == null)
@@ -74,19 +81,18 @@ String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", 
             for (String type : tokenType) {
                 TableView<LexicalAnalyzer.Token> table = createTable(type);
                 if (table != null){
-                    Label label = new Label(type);
-                    label.setStyle("-fx-font-weight: bold; -fx-font-size: 15;");
-                    vboxView.getChildren().add(label);
                     vboxView.getChildren().add(table);
                 }
             }
         }
     }
+
     private TableView<LexicalAnalyzer.Token> createTable(String type){
         TableView<LexicalAnalyzer.Token> tokensTable = new TableView<>();
-        tokensTable.setMinHeight(200);
-        tokensTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        TableColumn<LexicalAnalyzer.Token, String> lexemeCol = new TableColumn<>("Lexemes");
+        tokensTable.setMinHeight(250);
+        tokensTable.getStyleClass().add("table-view");
+        tokensTable.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+        TableColumn<LexicalAnalyzer.Token, String> lexemeCol = new TableColumn<>(type);
         lexemeCol.setCellValueFactory(new PropertyValueFactory<>("Lexeme"));
         lexemeCol.setResizable(true);
         lexemeCol.setEditable(false);
@@ -96,6 +102,16 @@ String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", 
                 tokensTable.getItems().add(token);
             }
         }
+        // Check if the content of the cells in the column exceeds the width of the column
+        lexemeCol.widthProperty().addListener((obs, oldVal, newVal) -> {
+            for (LexicalAnalyzer.Token item : tokensTable.getItems()) {
+                if (lexemeCol.getCellData(item) != null && lexemeCol.getCellData(item).length() > newVal.doubleValue()) {
+                    lexemeCol.setMinWidth(Control.USE_PREF_SIZE);
+                    return;
+                }
+            }
+            lexemeCol.setMinWidth(349);
+        });
         if (!tokensTable.getItems().isEmpty()) {
             tokensTable.getColumns().add(lexemeCol);
             return tokensTable;
@@ -107,6 +123,7 @@ String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", 
     void parseCode(MouseEvent event) {
 
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         fileChooser.setInitialDirectory(new File("../../"));
@@ -114,5 +131,21 @@ String [] tokenType = {"Keyword", "Identifier", "Operator", "Integer", "Float", 
             lineArea.setScrollTop(newValue.doubleValue());
         });
         viewWindow.getStyleClass().add("hideView");
+        codeArea.getStyleClass().add("text-area");
+        codeArea.layoutBoundsProperty().addListener(new ChangeListener<Bounds>() {
+        @Override
+        public void changed(ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) {
+            Node content = codeArea.lookup(".content");
+            if (content != null && !codeArea.getText().isEmpty()) {
+                double contentWidth = content.getBoundsInLocal().getWidth();
+                double textAreaWidth = newValue.getWidth();
+                if (contentWidth > textAreaWidth-6) {
+                    lineArea.setPadding(new Insets(0, 0, 13, 0));
+                } else {
+                    lineArea.setPadding(new Insets(0, 0, 0, 0));
+                }
+            }
+        }
+    });
     }
 }

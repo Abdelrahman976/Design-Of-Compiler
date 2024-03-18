eb74750d4cf519532790a6915c9217b73024e6f4
Abdelrahman976 <abdelrahmanbarakat976@gmail.com>
2024-03-19 00:27:38 +0200

    fixed error in regex pattern in process line

--- Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/LexicalAnalyzer.java
+++ Java Project/Lexical Analyzer/src/main/java/com/compiler/lexicalanalyzer/LexicalAnalyzer.java
@@ -312,7 +312,7 @@ public class LexicalAnalyzer {
     private String processLine(String line) {
         String[] inlineParts = line.split("//", -1);
         String noComments = inlineParts[0]; // Return only the part before the inline comment
-        String[] stringLiteralParts = noComments.split("\".*?\"|'.*?'", -1);
+        String[] stringLiteralParts = noComments.split("\".*?\\\".*?\\\".*?\"|\".*?\"|'.*?'", -1);
         StringBuilder noStringLiterals = new StringBuilder();
         for (String part : stringLiteralParts) {
             noStringLiterals.append(part);
@@ -355,8 +355,8 @@ public class LexicalAnalyzer {
         String directory = "D:\\Semester 6\\Design of Compilers\\Project\\TestCases\\Test6.c";
         analyzer.tokenize(directory);
         analyzer.typeOf();
-        analyzer.printTokens();
-//        analyzer.SymbolTableMaker(directory);
-//        analyzer.printSymbolTable();
+//        analyzer.printTokens();
+        analyzer.SymbolTableMaker(directory);
+        analyzer.printSymbolTable();
     }
 }

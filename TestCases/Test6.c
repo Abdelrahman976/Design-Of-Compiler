a294fe1828df955fdbf8ce6600c2418c72bd41ab
Abdelrahman976 <abdelrahmanbarakat976@gmail.com>
2024-03-18 22:20:09 +0200

    edited on Test6

--- TestCases/Test6.c
+++ TestCases/Test6.c
@@ -1,11 +1,10 @@
-
-//printf("printf(\"Hello, world!\\n\")");
-//int main(int argc,char const*argv[]) {
-//    int fortytwo = 0x2A, int0 = 0b0, int1 = 0b1, int2 = 02;
-//    do { int0 += int1++ + int2; } while (--fortytwo > 1);
-//    double doublewoh = (double) int0/*hi there*// (int1 += int2);
-//    int hamda123hamada123 = 2;
-//}
+int main(int argc,char const*argv[]) {
+    int fortytwo = 0x2A, int0 = 0b0, int1 = 0b1, int2 = 02;
+    do { int0 += int1++ + int2; } while (--fortytwo > 1);
+    double doublewoh = (double) int0/*hi there*// (int1 += int2);//hello world
+    int hamda123hamada123 = 2;
+}
+int valid_binary=0b010101;
 int invalid_binary_1 = 0b12; // Invalid, interpreted as octal, not binary
 int invalid_binary_2 = 1b023; // Pretending to be binary but is just decimal 10
 
@@ -19,3 +18,4 @@ int valid_hex_2 = 0X4D2; // Valid
 int valid_octal_1 = 0754; // Valid
 int valid_octal_2 = 042; // Valid
 int invalid_octal_1 = 089; // Invalid in C, will cause a compilation error
+printf("printf(\"Hello, world!\\n\")");
